bd09efcd90fdb285bf73062d5bf43bbb5a62974b
Caio Oliveira <caiobo@lsd.ufcg.edu.br>
2017-02-13 19:01:03 -0300

    lab 3 first part

--- lab3/lab3-script.R
+++ lab3/lab3-script.R
@@ -1,15 +1,20 @@
 
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
-training.data.raw <- read.csv('/home/caiobo/data-analysis-2/lab3/treino_classificacao.csv',header=T)
+training.data.raw <- read.csv('~/data-analysis-2/lab3/treino_classificacao.csv',header=T)
 
 plotado <- training.data.raw %>% 
   ggplot(aes(MAT_TUR_ANO, EVADIU)) +
     geom_point(shape = 21, colour = "black", fill = "white", size = 5, stroke = 5)
+plotado
 
-library(plotly)
+data2 <- training.data.raw %>%
+  group_by(MAT_TUR_ANO) %>%
+  summarise(EVA = sum(EVADIU == TRUE), NAO_EVADIU = sum(EVADIU==FALSE))
+
 set.seed(100)
-d <- diamonds[sample(nrow(diamonds), 1000), ]
-plot_ly(d, x = ~carat, y = ~price, color = ~carat,
-        size = ~carat, text = ~paste("Clarity: ", clarity))
+
+plot_ly(data2, x = ~MAT_TUR_ANO, y = ~EVA, color = ~EVA,
+        size = ~EVA)
